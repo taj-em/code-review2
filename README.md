a4aa7d7906b06de9b0afe32d9183bdc3044bac7c
Your Name <tajempey@gmail.com>
2023-10-20 17:22:30 -0600

    Add README and format code

---
+++ README.md
@@ -0,0 +1,35 @@
+# _Language Picker_
+
+#### By _**Taj Empey**_
+
+#### _Decides your programming language based on your answers_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _BootStrap_
+
+## Description
+
+_Uses a form to gather input based on user's answers.  Uses branching to determine the user's programming language._
+
+## Setup/Installation Requirements
+
+* _Clone repository from Github_
+* _Open index.html in a browser_
+
+## Known Bugs
+
+* _N/A_
+
+## License
+
+Copyright (c) _10/20/23_ _Taj Empey_
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-function yourLanguage(event) {
-  event.preventDefault();
-  const answer1 = parseInt(document.querySelector("input[name='question1']:checked").value);
-  const answer2 = parseInt(document.querySelector("input[name='question2']:checked").value);
-  const answer3 = parseInt(document.querySelector("input[name='question3']:checked").value);
-  const answer4 = parseInt(document.querySelector("input[name='question4']:checked").value);
-  const answer5 = parseInt(document.querySelector("input[name='question5']:checked").value);
-
-
+function calculate(answer1, answer2, answer3, answer4, answer5) {
   let runningCount = 0;
   if (answer1 == 1) {
     runningCount -= 1
@@ -15,7 +7,7 @@ function yourLanguage(event) {
   } else if (answer1 == 3) {
     runningCount += 1
   }
-
+  
   if (answer2 == 1) {
     runningCount -= 1
   } else if (answer2 == 2) {
@@ -23,7 +15,7 @@ function yourLanguage(event) {
   } else if (answer2 == 3) {
     runningCount += 1
   }
-
+  
   if (answer3 == 1) {
     runningCount -= 1
   } else if (answer3 == 2) {
@@ -31,7 +23,7 @@ function yourLanguage(event) {
   } else if (answer3 == 3) {
     runningCount += 1
   }
-
+  
   if (answer4 == 1) {
     runningCount -= 1
   } else if (answer4 == 2) {
@@ -39,7 +31,7 @@ function yourLanguage(event) {
   } else if (answer4 == 3) {
     runningCount += 1
   }
-
+  
   if (answer5 == 1) {
     runningCount -= 1
   } else if (answer5 == 2) {
@@ -47,6 +39,7 @@ function yourLanguage(event) {
   } else if (answer5 == 3) {
     runningCount += 1
   }
+  
   let img = document.getElementById("img");
   let yourLanguage;
   let languageDescription;
@@ -63,51 +56,70 @@ function yourLanguage(event) {
     img.src = "img/javascript.png"
     languageDescription = "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2023, 98.7% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries."
   }
-  document.getElementById("languageDescription").innerText = languageDescription;
-  document.getElementById("yourLanguage").innerText = yourLanguage;
-  document.getElementById("display").classList.remove("hide-display");
+  display(languageDescription, yourLanguage);
+  }
+
+
+
+
+
+
+function gatherInput(event) {
+  event.preventDefault();
+const answer1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+const answer2 = parseInt(document.querySelector("input[name='question2']:checked").value);
+const answer3 = parseInt(document.querySelector("input[name='question3']:checked").value);
+const answer4 = parseInt(document.querySelector("input[name='question4']:checked").value);
+const answer5 = parseInt(document.querySelector("input[name='question5']:checked").value);
+calculate(answer1, answer2, answer3, answer4, answer5);
 }
 
-  function reset(event) {
-    location.reload();
-  }
-  
-  function darkMode(event) {
-    document.querySelector("body").style.backgroundColor = "black";
-    document.querySelector("body").style.color = "white";
-    document.getElementById("img").style.borderColor = "white";
-    document.getElementById("q1-title").style.textDecorationColor = "#8948E9";
-    document.getElementById("q2-title").style.textDecorationColor = "#8948E9";
-    document.getElementById("q3-title").style.textDecorationColor = "#8948E9";
-    document.getElementById("q4-title").style.textDecorationColor = "#8948E9";
-    document.getElementById("q5-title").style.textDecorationColor = "#8948E9";
-    document.getElementById("sub-title1").style.textDecorationColor = "#8948E9";
-    document.getElementById("sub-title2").style.textDecorationColor = "#8948E9";
-    document.getElementById("reset-btn").style.backgroundColor = "#8948E9";
-    document.getElementById("darkMode-btn").style.display = "none";
-    document.getElementById("lightMode-btn").style.display = "initial"
-  }
+function display(languageDescription, yourLanguage) {
+document.getElementById("languageDescription").innerText = languageDescription;
+document.getElementById("yourLanguage").innerText = yourLanguage;
+document.getElementById("display").classList.remove("hide-display");
+}
 
-  function lightMode(event) {
-    document.querySelector("body").style.backgroundColor = "white";
-    document.querySelector("body").style.color = "black";
-    document.getElementById("img").style.borderColor = "black";
-    document.getElementById("q1-title").style.textDecorationColor = "#852C15";
-    document.getElementById("q2-title").style.textDecorationColor = "#852C15";
-    document.getElementById("q3-title").style.textDecorationColor = "#852C15";
-    document.getElementById("q4-title").style.textDecorationColor = "#852C15";
-    document.getElementById("q5-title").style.textDecorationColor = "#852C15";
-    document.getElementById("sub-title1").style.textDecorationColor = "#852C15";
-    document.getElementById("sub-title2").style.textDecorationColor = "#852C15";
-    document.getElementById("reset-btn").style.backgroundColor = "#852C15";
-    document.getElementById("lightMode-btn").style.display = "none";
-    document.getElementById("darkMode-btn").style.display = "initial"
-  }
+function reset(event) {
+  location.reload();
+}
+
+function darkMode(event) {
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector("body").style.color = "white";
+  document.getElementById("img").style.borderColor = "white";
+  document.getElementById("q1-title").style.textDecorationColor = "#8948E9";
+  document.getElementById("q2-title").style.textDecorationColor = "#8948E9";
+  document.getElementById("q3-title").style.textDecorationColor = "#8948E9";
+  document.getElementById("q4-title").style.textDecorationColor = "#8948E9";
+  document.getElementById("q5-title").style.textDecorationColor = "#8948E9";
+  document.getElementById("sub-title1").style.textDecorationColor = "#8948E9";
+  document.getElementById("sub-title2").style.textDecorationColor = "#8948E9";
+  document.getElementById("reset-btn").style.backgroundColor = "#8948E9";
+  document.getElementById("darkMode-btn").style.display = "none";
+  document.getElementById("lightMode-btn").style.display = "initial"
+}
+
+function lightMode(event) {
+  document.querySelector("body").style.backgroundColor = "white";
+  document.querySelector("body").style.color = "black";
+  document.getElementById("img").style.borderColor = "black";
+  document.getElementById("q1-title").style.textDecorationColor = "#852C15";
+  document.getElementById("q2-title").style.textDecorationColor = "#852C15";
+  document.getElementById("q3-title").style.textDecorationColor = "#852C15";
+  document.getElementById("q4-title").style.textDecorationColor = "#852C15";
+  document.getElementById("q5-title").style.textDecorationColor = "#852C15";
+  document.getElementById("sub-title1").style.textDecorationColor = "#852C15";
+  document.getElementById("sub-title2").style.textDecorationColor = "#852C15";
+  document.getElementById("reset-btn").style.backgroundColor = "#852C15";
+  document.getElementById("lightMode-btn").style.display = "none";
+  document.getElementById("darkMode-btn").style.display = "initial"
+}
 
 window.addEventListener("load", function () {
   let form = document.querySelector("form");
   document.getElementById("reset-btn").addEventListener("click", reset)
   document.getElementById("darkMode-btn").addEventListener("click", darkMode)
   document.getElementById("lightMode-btn").addEventListener("click", lightMode)
-  form.addEventListener("submit", yourLanguage)
+  form.addEventListener("submit", gatherInput)
 });
